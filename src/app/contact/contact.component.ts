16f66029797284ad9b416a948b7da6880a5fed8e
Albert <wissigkeit@outlook.de>
2023-07-31 16:17:09 +0200

    change contact function, delete/add  images

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,38 +12,60 @@ export class ContactComponent {
   @ViewChild('messageField') messageField!: ElementRef;
   @ViewChild('sendButton') sendButton!: ElementRef;
 
-  async sendMail() {
-    console.log('sending mail', this.myForm);
-    // this.myForm.nativeElement.disable();
-    let nameField = this.nameField.nativeElement;
-    let emailField = this.emailField.nativeElement;
-    let messageField = this.messageField.nativeElement;
-    let sendButton = this.sendButton.nativeElement;
 
-    nameField.disabled = true;
-    emailField.disabled = true;
-    messageField.disabled = true;
-    sendButton.disabled = true;
-    // animation  für senden anzeigen
+  async sendMail() {
+    const { nameField, emailField, messageField, sendButton } = this.getData();
+    this.disableForm(nameField, emailField, messageField, sendButton);
     let formData = new FormData();
-    formData.append('name', nameField.value);
-    formData.append('email', emailField.value);
-    formData.append('message', messageField.value);
+    this.getFormData(formData, nameField, emailField, messageField);
     await fetch('https://albert-wissigkeit.developerakademie.net/portfolio/send_mail.php',
       {
         method: 'POST',
         body: formData
       }
     );
-    sendButton.style.backgroundColor = 'gray';
     setTimeout(() => {
-      sendButton.style.backgroundColor = '';
-      nameField.value = '';
-      emailField.value = '';
-      messageField.value = '';
+      this.clearForm(nameField, emailField, messageField, sendButton);
       this.enableFormFields(nameField, emailField, messageField, sendButton);
     }, 5000);
   }
+
+
+  getData() {
+    return {
+      nameField: this.nameField.nativeElement,
+      emailField: this.emailField.nativeElement,
+      messageField: this.messageField.nativeElement,
+      sendButton: this.sendButton.nativeElement,
+    };
+  }
+
+
+  clearForm(nameField: { value: string; }, emailField: { value: string; }, messageField: { value: string; }, sendButton: { style: { backgroundColor: string; }; }) {
+    sendButton.style.backgroundColor = '';
+    nameField.value = '';
+    emailField.value = '';
+    messageField.value = '';
+  }
+
+
+  getFormData(formData: FormData, nameField: { value: any; }, emailField: { value: any; }, messageField: { value: any; }) {
+    formData.append('name', nameField.value);
+    formData.append('email', emailField.value);
+    formData.append('message', messageField.value);
+  }
+
+
+  disableForm(nameField: { disabled: boolean; }, emailField: { disabled: boolean; }, messageField: { disabled: boolean; }, sendButton: {
+    style: any; disabled: boolean;
+  }) {
+    sendButton.style.backgroundColor = 'gray';
+    nameField.disabled = true;
+    emailField.disabled = true;
+    messageField.disabled = true;
+    sendButton.disabled = true;
+  }
+
 
   enableFormFields(nameField: { disabled: boolean; }, emailField: { disabled: boolean; }, messageField: { disabled: boolean; }, sendButton: { disabled: boolean; }) {
     nameField.disabled = false;
--- src/app/portfolio-part/portfolio-part.component.ts
+++ src/app/portfolio-part/portfolio-part.component.ts
@@ -8,28 +8,28 @@ import { Component } from '@angular/core';
 export class PortfolioPartComponent {
   projects = [
     {
-      img: './assets/img/portfolio/join.png',
+      img: './assets/img/jointest2gut.png',
       name: 'Join',
       code: 'JavaScript | HTML | CSS',
-      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      description: 'Task management tool inspired by the Kanban System. Easily create and organize tasks using drag-and-drop functions and assign them to users or categories.',
       projectlink: 'https://albert-wissigkeit.de/Join/',
       githublink: 'https://github.com/CrazyBird007/Join',
       reverse: false
     },
     {
-      img: './assets/img/portfolio/pollo-loco.png',
+      img: './assets/img/portfolio/pollo-loco-pc.png',
       name: 'El Pollo Loco',
       code: 'JavaScript | HTML | CSS',
-      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      description: 'Jump, run and throw game based on an object-oriented approach. Help Pepe collect coins and salsa to fight the crazy hen.',
       projectlink: 'https://albert-wissigkeit.de/ElPolloLoco/',
       githublink: 'https://github.com/CrazyBird007/PolloLoco',
       reverse: true
     },
     {
-      img: './assets/img/portfolio/pokedex.png',
+      img: './assets/img/pokedex-pc.png',
       name: 'Pokedex',
-      code: 'JavaScript | HTML | CSS | Api',
-      description: 'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
+      code: 'JavaScript | HTML | CSS | API',
+      description: 'Based on the PokéAPI, a simple library that provides and catalogs Pokémon information.',
       projectlink: 'https://albert-wissigkeit.de/PokeDex/',
       githublink: 'https://github.com/CrazyBird007/PokeDex',
       reverse: false
